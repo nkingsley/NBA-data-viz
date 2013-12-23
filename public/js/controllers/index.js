2025b8d72f2e9ee5cb60d48bc2e486282728f0f0
Santiago <santiago.archila@gmail.com>
2013-12-23 10:36:45 -0800

    testing data-bound HTML5 sliders

--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -1,3 +1,36 @@
 angular.module('mean.system').controller('IndexController', ['$scope', 'Global', function ($scope, Global) {
     $scope.global = Global;
-}]);
+    $scope.stealsWeight = 0;
+    $scope.blocksWeight = 0;
+
+    //Width and height
+    var w = 500;
+    var h = 100;
+
+    var dataset = [
+            [5, 20], [480, 90], [250, 50], [100, 33], [330, 95],
+            [410, 12], [475, 44], [25, 67], [85, 21], [220, 88]
+            ];
+
+    //Create SVG element
+    var svg = d3.select("body")
+          .append("svg")
+          .attr("width", w)
+          .attr("height", h);
+
+    svg.selectAll("circle")
+       .data(dataset)
+       .enter()
+       .append("circle")
+       .attr("cx", function(d) {
+          return d[0] + $scope.blocksWeight;
+       })
+       .attr("cy", function(d) {
+          return d[1] + $scope.stealsWeight;
+       })
+       .attr("r", function(d) {
+          return Math.sqrt(h - d[1]);
+       });
+
+
+  }]);
