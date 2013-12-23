fe9143ff2cce0362d4e7321b375df28cf726f03b
Santiago <santiago.archila@gmail.com>
2013-12-23 13:52:10 -0800

    add initial version of calculatPlayerStar

--- public/js/controllers/mainCtrl.js
+++ public/js/controllers/mainCtrl.js
@@ -1,10 +1,20 @@
 angular.module('mean.chart')
-  .controller('mainCtrl', function AppCtrl ($scope) {
+  .controller('mainCtrl', function AppCtrl ($scope, teamStats) {
     $scope.options = {width: 500, height: 300, 'bar': 'aaa'};
     $scope.data = [1, 2, 3, 4];
+    
     $scope.hovered = function(d){
       $scope.barValue = d;
       $scope.$apply();
     };
+
     $scope.barValue = 'None';
+
+    $scope.calculatePlayerStar = function (values, weights) {
+      var starStatistic = 0;
+      for (key in values){
+        starStatistic += (values[key]/leagueMedians[key])*weights[key];
+      }
+      return values["Total Min"]*starStatistic;
+    };
   });
