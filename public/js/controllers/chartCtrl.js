7d698502df0ac0a43c26b61a2132d72180e051c5
Wayne Montague <wayne.montague@gmail.com>
2014-01-04 14:32:55 -0800

    modified app layout so that chart controls now reside to the right of the chart

--- public/js/controllers/chartCtrl.js
+++ public/js/controllers/chartCtrl.js
@@ -1,7 +1,7 @@
 angular.module('mean.chart')
   .controller('chartCtrl', ['$scope', '$http', 'Global', 'Stats', function ($scope, $http, Global, Stats) {
     
-    $scope.options = {width: 940, height: 500};
+    $scope.options = {width: 840, height: 500};
     $scope.teams = Stats.teams;
     $scope.calculateAllTeamStarVals = Stats.calculateAllTeamStarVals;
     $scope.changeSliders = Stats.changeSliders;
