704cce512a1d68b7d6eb8a8c08c26015fe84df86
Santiago <santiago.archila@gmail.com>
2014-01-07 23:31:29 -0800

    add spearman rho service

---
+++ public/js/services/spearman.js
@@ -0,0 +1,30 @@
+angular.module('mean.chart').factory("Spearman", ['Global', 'Stats', function (Global, Stats) {
+
+  var rankBy = function (theTeams, rankBy){
+    theTeams.sort(function(a, b){
+      return a[rankBy]-b[rankBy];
+    });
+    for (var i = 0; i < theTeams.length; i++) {
+      theTeams[i][rankBy + "Rank"] = i+1;
+    }
+  };
+
+  rankBy(Stats.teams,"winPct");
+
+  var spearmanRho = function (){
+    rankBy(Stats.teams,"starVal");
+    Stats.teams.forEach(function (team){
+      team.di = team.starValRank - team.winPctRank;
+      team.di2 = Math.pow(team.di,2);
+    });
+
+    var sum = Stats.teams.reduce(function(seed, eachTeam){
+      return seed + eachTeam.di2;
+    }, 0);
+    var n = Stats.teams.length;
+    var rho = 1 - (6*sum)/(Math.pow(n,3)-n);
+
+    return rho;
+  };
+  
+}]);
